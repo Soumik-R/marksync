429dcdcac58b2702df19cf36523d5f6a7bcec280
SOUMIK ROY <soumikroy7272@gmail.com>
2026-02-14 20:12:49 +0530

    Fixed login issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,7 +82,12 @@ export default function Home() {
 
 
   const login = async () => {
-    await supabase.auth.signInWithOAuth({ provider: "google" });
+    await supabase.auth.signInWithOAuth({ 
+      provider: "google",
+      options: {
+        redirectTo: `${window.location.origin}/`
+      }
+    });
   };
 
   const logout = async () => {
